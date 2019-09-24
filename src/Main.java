bddcc0b6ca04bbbf71ade2f620129405ffac002c
mohammed el-orfali <Moel18ad@student.cbs.dk>
2019-09-24 09:26:26 +0200

    message

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         String myUsername = "hej";
         String myPassword = "hej";
+        System.out.println("Hahaha nizaaaahr");
 
         if(!User.validateUsername(myUsername)) {
             System.out.println("Username doesn't fit the rules. Please check the User class.");
