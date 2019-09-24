933c50cfbaa87864cea08dc752125fe221752500
Waseem Ur Rehman <waseemurrehman@Waseems-MacBook-Air-2.local>
2019-09-24 09:33:27 +0200

    hej

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         String myUsername = "hej";
-        String myPassword = "hej";
+        String myPassword = "hej123";
 
         if(!User.validateUsername(myUsername)) {
             System.out.println("Username doesn't fit the rules. Please check the User class.");
